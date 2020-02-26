5db43f05a1446e567fe922918811ff01b51f5231
lokeshk9797 <lokeshkashyap858@gmail.com>
2020-02-26 17:00:18 +0530

    Factory Pattern Implemented

---
+++ Factory.Solution/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory.Solution
+{
+    public class Point
+    {
+        private double x, y;
+        private Point(double x,double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho*Math.Cos(theta), rho*Math.Sin(theta));
+        }
+
+        public override string ToString()
+        {
+            return $"x:{x}, y:{y}";
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var point = Point.NewPolarPoint(2*Math.Sqrt(2),(3*Math.PI)/4);
+            Console.WriteLine(point);
+        }
+    }
+}
