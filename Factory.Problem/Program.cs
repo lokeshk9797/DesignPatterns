12b6f860b3b149bf442ae026ed1850bd099d6e47
lokeshk9797 <lokeshkashyap858@gmail.com>
2020-02-26 16:46:39 +0530

    Explored Problems that need Factory Builder

---
+++ Factory.Problem/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory.Problem
+{
+    using System;
+
+    namespace DotNetDesignPatternDemos.Creational.Factories
+    {
+        
+
+        public class Point
+        {
+
+            private double x, y;
+            public enum CoordinateSystem
+            {
+                Cartesian,
+                Polar
+            }
+            public Point(double a,
+              double b, // names do not communicate intent
+              CoordinateSystem cs = CoordinateSystem.Cartesian)
+            {
+                switch (cs)
+                {
+                    case CoordinateSystem.Polar:
+                        x = a * Math.Cos(b);
+                        y = a * Math.Sin(b);
+                        break;
+                    default:
+                        x = a;
+                        y = b;
+                        break;
+                }
+            }
+
+            public override string ToString()
+            {
+                return $"x:{x} , y: {y}";
+            }
+
+           
+        }
+        class Demo
+        {
+            static void Main(string[] args)
+            {
+                var p1 = new Point(2, 3, Point.CoordinateSystem.Cartesian);
+                Console.WriteLine(p1);
+                
+            }
+        }
+    }
+
+}
